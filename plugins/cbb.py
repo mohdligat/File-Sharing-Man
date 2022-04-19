9b310c69dfa11d319c170de012711fbbc65ac0c8
mohdligat <103974192+mohdligat@users.noreply.github.com>
2022-04-19 10:40:00 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,11 +1,11 @@
-# read all my data u will find the answer
-# share to all but make adjustment here n there
-# t.me/UnlimitedTrusted & t.me/TekanSiniLah
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
 
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import OWNER
 
 
 @Bot.on_callback_query()
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://t.me/IWasNotABot'>Klik Disini</a>\n • Owner Repo: @IWasNotABot</b>\n",
+            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
